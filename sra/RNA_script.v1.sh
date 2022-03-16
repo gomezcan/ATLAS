4f6efa94c0d859d84a57bd5e9d391b51f8849936
gomezcan <46493296+gomezcan@users.noreply.github.com>
2022-03-16 11:26:29 -0400

    Update RNA_script.v1.sh
    update changes

--- sra/RNA_script.v1.sh
+++ sra/RNA_script.v1.sh
@@ -1,44 +1,36 @@
 #!/bin/bash
 
-########## BATCH Lines for Resource Request ##########
-#SBATCH --time=02:00:00          	# Limit of wall clock time - how long the job will run (same as -t)
-#SBATCH --nodes=1            		# Number of different nodes - could be an exact number or a range of nodes (same as -N)
-#SBATCH --ntasks=1             		# Number of tasks - how many tasks (nodes) that you require (same as -n)
-#SBATCH --cpus-per-task=100      	# Number of CPUs (or cores) per task (same as -c)
-#SBATCH --mem=1${threads}G               	# Specify the real memory required per node. (20G)
-#SBATCH --job-name Mapping      # You can give your job a name for easier identification (same as -J)
-#SBATCH -A egl 
-########## Load Modules #########
-
-module purge
-module load GCCcore/8.2.0
-module load GGCcore/4.7.2
-module load hisat2/2.1.0
-module load SAMtools/1.11
-module load parallel-fastq-dump/0.6.5-Python-3.7.2
-module load Trimmomatic/0.39-Java-11
-module load FastQC/0.11.7-Java-1.8.0_162
+## Modules required 
+#module purge
+#module load GCCcore/8.2.0
+#module load GGCcore/4.7.2
+#module load hisat2/2.1.0
+#module load SAMtools/1.11
+#module load parallel-fastq-dump/0.6.5-Python-3.7.2
+#module load Trimmomatic/0.39-Java-11
+#module load FastQC/0.11.7-Java-1.8.0_162
 
 ################### Script ###################
 
 ######## Choose additional processes #######
 
 processing = true;		## Enabling the processes from downloading to read mapping
-fastqc_report = true;           ## Ask the fastqc report for every enabled filter process
-Q20 = true;			## Filter the mapped reads by 20 quality
+fastqc_report = false;           ## Ask the fastqc report for every enabled filter process
+Q20 = false;			## Filter the mapped reads by 20 quality
 Unique_mapping = false;		## Delete the multimapping reads
 Drop_Duplicates = false;	## Drop the duplicated reads
 DeDuplicates_Q20 false;		## Drop the duplicated Q20 reads | Remember to set Q20 variable to "true"
 
 ####### Reference Genome #######
 
+*** Pending index condition
 path_genome = "path/file/genome";   # Input the Reference genome path file
 
 ####### Paired or single ends #######
 
 list_file = "file_list/path/file";	## Input the donwload list path file
 
-read -r firtsline < ${list_file};	## Read the first line on the list
+*** read -r firtsline < ${list_file};	## Read the first line on the list
 
 
 ####### Number of threads #######
@@ -47,30 +39,35 @@ threads = 50;	# It's a recurrent parameter
 
 ####### Choose the way #######
 
-if [${firstline[1]} == "Single"]
-
-
 ###################### SINGLE END SCRIPT ##########################
+#### Genome Indexing ####
+	
+# hisat2-build ${path_genome} index_genome;
 
-then
-
+if [ ${firstline[1]} == "Single" ]
+	then
 	#### Donwload the data####
 
 	if ${processing}; then
 
 	while read -r line; do 
-	parallel-fastq-dump --gzip -t ${threads} -s $line -O .;
-	done < ${list_file};
-
-	#### Clean with trimmomatic ####
-
-	for file in *fastq.gz; do
-	java -jar trimmomatic-0.39.jar SE -t ${threads} $file Clean.$file ILLUMINACLIP:Adapter.fastq:2:30:10 SLIDINGWINDOW:4:20 MINLEN:30;
-	done; 
-
-	#### Genome Indexing ####
+	parallel-fastq-dump --gzip -t ${threads} -s $line -O . ;
 	
-	hisat2-build ${path_genome} index_genome;
+	echo 'cleaning' $line
+	
+	java -jar trimmomatic-0.39.jar SE -t ${threads} ${line}.fastq.gz Clean.${line}.fastq.gz ILLUMINACLIP:Adapter.fastq:2:30:10 SLIDINGWINDOW:4:20 MINLEN:30;
+	echo 'mapping' $line
+	hisat2 -p ${threads} --no-unal --add-chrname --no-mixed --no-discordant -x index_genome -U $file -S $file_out;
+	
+	samtools view -@ ${threads} -h -S -b ${file_out} | samtools sort -@ ${threads} -o sorted.${file_out//.sam/.bam};
+	
+	# $line.fastq.gz
+	# $line_1.fastq.gz
+	# $line_2.fastq.gz
+
+	done; 
+	./my_counter
+
 
 	#### Read Mapping ####
 
@@ -78,8 +75,7 @@ then
 	file_out = ${file//Clean./};
 	file_out = ${file_out//fastq.gz/.sam};
 	hisat2 -p ${threads} --no-unal --add-chrname --no-mixed --no-discordant -x index_genome -U $file -S $file_out;
-
-	samtools view -@ ${threads} -h -S -b ${file_out} | samtools sort -@ ${threads} -o sorted.${file_out//.sam/.bam};
+	samtools view -@ ${threads} -h -S -b ${file_out} | samtools sort -@ ${threads} -o ${file_out//.sam/.bam};
 	done;
 
 	fi;
@@ -246,3 +242,6 @@ else
 
         fi;
 fi
+
+# Note
+#for i in SRR_*; do n=$(wc -l $i | cut -d' ' -f1); echo $n | awk -v f=$i '{print f, $1/4, $1/4}' ; done;
