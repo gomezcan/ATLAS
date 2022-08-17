701152bee5b71daeb798c62be3dc6f605094ee2a
camagomezcan <30728981+camagomezcan@users.noreply.github.com>
2022-08-17 15:07:37 -0400

    Update Normalization_TPM_TMM_CTF.R
    Only executes one of the three normalization methods; and change the data's input way.

--- src/Normalization_TPM_TMM_CTF.R
+++ src/Normalization_TPM_TMM_CTF.R
@@ -2,12 +2,15 @@
 library(foreach)
 library(doParallel)## Two parallelization packages
 library(edgeR)
-args <- commandArgs(trailingOnly = TRUE)
 
+############################
+args <- commandArgs( TRUE) ## Input Counts and stats
+methods <- c('TMM','CTF','TPM')  ## Select the method
+method <- methods[2] ## SELECT ONE OF THE THREE METHODS
 ########### Import Data##############
 
-counts <- read.table(file='..', header = T, sep = '\t')
-counts_stat <- read.table(file='..', head=TRUE)
+counts <- read.table(file = args[1], header = T, sep = '\t')
+counts_stat <- read.table(file = args[2], head=TRUE)
 genes <- counts[,1]
 Samples <- colnames(counts_stat)[-1]
 
@@ -19,7 +22,7 @@ std <- sd(x=percents[-1]) ## standard deviation
 mu <- mean(as.numeric(percents[-1])) # Mean
 
 # It's gonna remove the outliers
-removed_samples <- percents[, percents >= Q1] # It chose Q1 but but could be use mu-2*std
+removed_samples <- percents[, percents >= Q1] # It chose Q1 but could be use mu-2*std
 
 #### Rename removed_samples columns: Removing the ".bam" pattern
 colnames(removed_samples) <- gsub(pattern='*.bam', replacement = "", colnames(removed_samples))
@@ -28,13 +31,14 @@ colnames(removed_samples) <- gsub(pattern='*.bam', replacement = "", colnames(re
 ### Getting the filtered data
 new_counts <- subset(counts, select = c(colnames(removed_samples)[-1])) ### Counts to be normalized
 new_counts <- counts[[colnames(removed_samples)[-1]]]
+
 ########################## MA-plots before normalization #####
 
 maPlot(new_counts[,1], new_counts[,200], deCol = 'green')
 abline(h=0, col = 'red')
 
 
-########################## Normaization ################################
+########################## Normalization ################################
 
 library_size <- counts_stat[1,][-1] ## Total Reads Assigned by sample vector
 colnames(library_size) <- gsub(pattern='*.bam', replacement = "", colnames(library_size)) ## Remove ".bam"
@@ -62,7 +66,7 @@ TPM <- function(sample){
 
 CTF <- function(Counts){
   ### Calculate the normalization factor for every sample
-  factors_norm <- calcNormFactors(Counts, library_size = library_size, method = 'TMM')
+  factors_norm <- calcNormFactors(Counts, lib_size = library_size, method = 'TMM')
   ### Normalize the counts
   counts_norm <- sweep(Counts, 2, factors_norm, FUN = '/')
   return(counts_norm)
@@ -74,32 +78,42 @@ CTF <- function(Counts){
 
 TMM <- function(Counts){
   ### Calculate the normalization factor for every sample
-  factors_norm <- calcNormFactors(Counts, library_size = library_size, method = 'TMM')
+  factors_norm <- calcNormFactors(Counts, lib_size = library_size, method = 'TMM')
   ### Effective size
-  effective_size <- factors_norm*as.numeric(library_size)
+  effective_size <- factors_norm*as.numeric(library_size)/10**6
   ### Normalize the counts
   counts_norm <- sweep(Counts, 2, effective_size, FUN = '/')
   return(counts_norm)
 }
 
-## Execute TPM
-system.time( tpm <- mapply(TPM, colnames(new_counts)))
+##################### DEFINE THE NORMALIZATIONFUNCTION ##############
 
-## Paralleization option, much faster than mapply
-numCores <- detectCores()
-registerDoParallel(numCores)
+Normalization <- function(method, X){
+  if(method=='TMM'){
+    X_normed <- TMM(X)
+  }
+  
+  else if (method=='CTF'){
+    X_normed <- CTF(X)
+  }
+  
+  else{
+    ## Parallelization option, much faster than mapply
+    numCores <- detectCores()
+    registerDoParallel(numCores)
+    
+    l <- length(ncol(new_counts))
+    
+      X_normed <- foreach (i = 1:l, .combine = cbind) %dopar% {
+      TPM(colnames(new_counts)[i])
+    }
+  }
+  return(X_normed)
+}
+###################### EXECUTING ##########################
 
-l <- length(ncol(new_counts))
-system.time(norm_tpm_counts <- foreach (i = 1:l, .combine = cbind) %dopar% {
-  TPM(colnames(new_counts)[i])
-})
-## Execute CTF
-
-ctf <- CTF(new_counts)
-
-## Execute TMM
-tmm <- TMM(new_counts)
+Counts_normalized <- Normalization(new_counts)
 
 
-maPlot(tmm[,1], tmm[,200],deCol = 'green') ## Plot mat plot with ctf normalization
+maPlot(Counts_normalized[,1], Counts_normalized[,200],deCol = 'green') ## Plot mat plot with tmm normalization
 abline(h=0, col = 'red')
